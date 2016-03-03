86cc2a203b392cd8474d82960878e7f68db77762
謝亞涵 <karen.hsieh@livebricks.com>
2016-03-03 18:22:03 +0800

    增加 foodmap 專案

---
+++ GAME/foodmap/index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="js/jquery.tinyMap-3.3.15.js"></script>
+</head>
+
+<style>
+
+</style>
+
+<body>
+
+    <div class="row">
+        <div class="col-xs-12 col-md-12">
+        </div>
+    </div>
+
+</body>
+</html>
+
+
+
+<script type="text/javascript">
+
+    $(document).ready(function() {
+
+    });
+</script>
